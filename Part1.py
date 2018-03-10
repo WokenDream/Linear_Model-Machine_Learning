3679c8a5798309e530f0719e187197fa3b943e9f
Tianqi Liu <tianqi.liu@mail.utoronto.ca>
2018-03-10 01:19:29 -0500

    code refactor for part 1_1

--- Part1.py
+++ Part1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 def buildGraph(learning_rate=0.005):
     # straight from tutorial ex3
-    W = tf.Variable(tf.truncated_normal(shape=[28*28, 1], stddev=0.5), name='W')
+    W = tf.Variable(tf.truncated_normal([28*28, 1], stddev=0.5), name='W')
     b = tf.Variable(0.0, name='b')
     X = tf.placeholder(tf.float32, [None, 28*28], name='X')
     y = tf.placeholder(tf.float32, [None, 1], name='y')
@@ -21,8 +21,7 @@ def buildGraph(learning_rate=0.005):
 
     return W, b, X, y, lamda, y_hat, mse, train
 
-
-if __name__ == "__main__":
+def part_1_1():
     with np.load("notMNIST.npz") as data:
         Data, Target = data["images"], data["labels"]
         posClass = 2
@@ -39,7 +38,6 @@ if __name__ == "__main__":
         trainData, trainTarget = Data[:3500], Target[:3500]
         validData, validTarget = Data[3500:3600], Target[3500:3600]
         testData, testTarget = Data[3600:], Target[3600:]
-
     print("trainData shape", trainData.shape)
     print("trainTarget shape", trainTarget.shape)
     print("validData shape", validData.shape)
@@ -47,22 +45,8 @@ if __name__ == "__main__":
     print("testData shape", testData.shape)
     print("testTarget shape", testTarget.shape)
 
-
-    W, b, X, y, lamda, y_hat, mse, train = buildGraph()
-
-    init = tf.global_variables_initializer()
-    sess = tf.InteractiveSession()
-    sess.run(init)
-
-    lamda_val = 0
-
-    W_list = []
-    train_error_list = []
-    valid_error_list = []
-    test_error_list = []
-
     batch_size = 500
-    num_iterations = 20000
+    num_iterations = 2000
     num_train = trainData.shape[0]
     num_batches = num_train // batch_size
     num_epochs = num_iterations // num_batches
@@ -71,42 +55,54 @@ if __name__ == "__main__":
     print("number of epochs:", num_epochs, "; epoch left-overs:", num_iterations_leftover)
     print("number of iterations:", num_iterations)
 
-    shuffled_inds = np.arange(num_train)
-    # follow lecture notes
-    for epoch in range(num_epochs):
-        # TODO: permute the order of training examples
-        # permuted_train_inds = np.random.choice(num_train, batch_size, replace=True)
-        np.random.shuffle(shuffled_inds)
+    for lr in [0.005, 0.001, 0.0001]:
+        W, b, X, y, lamda, y_hat, mse, train = buildGraph(lr)
+        lamda_val = 0
+        init = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(init)
+            W_list = []
+            train_error_list = []
 
-        temp_trainData = trainData[shuffled_inds]
-        temp_trainTargets = trainTarget[shuffled_inds]
-        for j in range(num_batches):
-            batch_trainData = temp_trainData[j * batch_size : (j + 1) * batch_size].reshape(batch_size, -1)
-            batch_targets = temp_trainTargets[j * batch_size : (j + 1) * batch_size]
-            _, err, currentW, currentb, yhat = sess.run([train, mse, W, b, y_hat], feed_dict={
-                X: batch_trainData,
-                y: batch_targets,
-                lamda: lamda_val
-            })
-        W_list.append(currentW)
-        train_error_list.append(err)
-        # print("epoch ", epoch, " train error: ", err)
+            shuffled_inds = np.arange(num_train)
+            # follow lecture notes
+            for epoch in range(num_epochs):
+                np.random.shuffle(shuffled_inds)
+                temp_trainData = trainData[shuffled_inds]
+                temp_trainTargets = trainTarget[shuffled_inds]
 
-    # np.random.shuffle(shuffled_inds)
-    # temp_trainData = trainData[shuffled_inds]
-    # temp_trainTargets = trainTarget[shuffled_inds]
-    # for iter in num_iterations_leftover:
-    #     batch_trainData = temp_trainData[iter * batch_size: (iter + 1) * batch_size].reshape(batch_size, -1)
-    #     batch_targets = temp_trainTargets[iter * batch_size: (iter + 1) * batch_size]
-    #     _, err, currentW, currentb, yhat = sess.run([train, mse, W, b, y_hat], feed_dict={
-    #         X: batch_trainData,
-    #         y: batch_targets,
-    #         lamda: lamda_val
-    #     })
-    # W_list.append(currentW)
-    # train_error_list.append(err)
-    # print("epoch ", epoch, " train error: ", err)
+                for j in range(num_batches):
+                    batch_trainData = temp_trainData[j * batch_size: (j + 1) * batch_size].reshape(batch_size, -1)
+                    batch_targets = temp_trainTargets[j * batch_size: (j + 1) * batch_size]
+                    _, err, currentW, currentb, yhat = sess.run([train, mse, W, b, y_hat], feed_dict={
+                        X: batch_trainData,
+                        y: batch_targets,
+                        lamda: lamda_val
+                    })
+                W_list.append(currentW)
+                train_error_list.append(err)
+                # print("epoch ", epoch, " train error: ", err)
 
-    plt.plot(np.arange(num_epochs), train_error_list)
+            # np.random.shuffle(shuffled_inds)
+            # temp_trainData = trainData[shuffled_inds]
+            # temp_trainTargets = trainTarget[shuffled_inds]
+            # for iter in num_iterations_leftover:
+            #     batch_trainData = temp_trainData[iter * batch_size: (iter + 1) * batch_size].reshape(batch_size, -1)
+            #     batch_targets = temp_trainTargets[iter * batch_size: (iter + 1) * batch_size]
+            #     _, err, currentW, currentb, yhat = sess.run([train, mse, W, b, y_hat], feed_dict={
+            #         X: batch_trainData,
+            #         y: batch_targets,
+            #         lamda: lamda_val
+            #     })
+            # W_list.append(currentW)
+            # train_error_list.append(err)
+            # print("epoch ", epoch, " train error: ", err)
+
+            plt.plot(np.arange(num_epochs), train_error_list)
     plt.title("SGD training - error vs epoch #")
+    plt.legend(['learning rate: 0.005', 'learning rate: 0.001', 'learning rate: 0.0001'])
     plt.show()
+
+
+if __name__ == "__main__":
+    part_1_1()
